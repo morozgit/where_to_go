d1a3d6106a9441a546e066dda146dafc0cc18cfa
morozgit <morozec76@gmail.com>
2023-10-13 15:58:12 +0300

    Uploaded JSON

--- places/views.py
+++ places/views.py
@@ -1,35 +1,49 @@
 from django.shortcuts import render
-
+from .models import Place
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, JsonResponse
+import os
+from django.conf import settings
 
 def show_map(request):
+    places = Place.objects.all()
+    features = []
+    for place in places:
+        features.append(
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [place.longitude, place.latitude]
+            },
+            "properties": {
+              "title": place.title,
+              "placeId": place.id,
+              "detailsUrl": 'https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json'
+            }
+          }
+        )
     places_geojson = {
       "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.62, 55.793676]
-          },
-          "properties": {
-            "title": "Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": 'https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json'
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": 'https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json'
-          }
-        }
-      ]
+      "features": features
     }
+    print(places_geojson)
     context = {'geojson': places_geojson}
     return render(request, 'index.html', context=context)
+
+
+def show_place(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    image_path = [place_image.image.url for place_image in place.images.all()]
+    place_data = {
+        "title": place.title,
+        "imgs": image_path,
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.longitude,
+            "lat": place.latitude
+        }
+      }
+
+    return JsonResponse(place_data, safe=False, json_dumps_params={'ensure_ascii': False , 'indent': 2})
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_map),
+    path('places/<int:place_id>/', views.show_place)
 ]
 
 if settings.DEBUG:
