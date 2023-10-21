d940c64614390dc1dcec4b301e6f1b955eb34d90
morozgit <morozec76@gmail.com>
2023-10-21 22:58:41 +0300

    add Image to admin

--- places/admin.py
+++ places/admin.py
@@ -1,14 +1,20 @@
-from adminsortable2.admin import SortableAdminBase, SortableTabularInline
+from adminsortable2.admin import (SortableAdminBase, SortableAdminMixin,
+                                  SortableTabularInline)
 from django.contrib import admin
 from django.utils.html import format_html
 
 from .models import Place, PlaceImage
 
 
+@admin.register(PlaceImage)
+class PlaceImageAdmin(SortableAdminMixin, admin.ModelAdmin):
+    list_display = ('place',)
+    autocomplete_fields = ['place']
+
+
 class PlaceImageInline(SortableTabularInline, admin.TabularInline):
     model = PlaceImage
     readonly_fields = ('get_preview',)
-    autocomplete_fields = ['place']
 
     def get_preview(self, image):
         return format_html('<img src="{}" style="max-height: 200px; max-width: auto;" />', image.image.url,)
